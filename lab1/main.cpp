8059b3672a87998833cc787fb6ec4bd849caf125
Nubia Balderas Ibarra <nubia.balderas-ibarra@g.austincc.edu>
2018-09-19 20:50:43 -0500

    lab 1

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -10,13 +10,17 @@
     int main()
     {
         double length1,length2,length3;
+        int a = length1
 
+        // do loop execution and ask user to enter 1st length
+        do {
+            cout<< "Please enter the length of first triangle side, a negative number will terminate:";
+            cin >> length1;
 
-        // Ask user to input one of the triangles side length
-        cout<< "Please enter the length of first triangle side:";
-        cin >> length1;
-        cout << "Please enter the second length of the triangle side:";
-        cin >> length2;
+        } while( a > 0 );
+        // Ask user to input the second triangles side length
+           cout << "Please enter the second length of the triangle side:";
+           cin >> length2;
 
         //Compute the hypotenuse
         length3= sqrt(pow(length1,2)+ pow(length2,2));
@@ -28,7 +32,6 @@
                 cout<< "The length of the hypotenuse of a right triangle is: " <<length3<< std::endl;
             }
         }
-
         std::cout << "Thanks for visiting!" << std::endl;
     return 0;
 }
