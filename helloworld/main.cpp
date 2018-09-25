544d153230135fcde804c3aada9d63050119018b
Nubia Balderas Ibarra <nubia.balderas-ibarra@g.austincc.edu>
2018-09-24 20:26:49 -0500

    hello worl project
    Here is my Hello world project

---
+++ helloworld/main.cpp
@@ -0,0 +1,18 @@
+// Nubia Balderas Ibarra
+// Cosc 1337
+//Hello World Homework
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+    string myname = "Nubia Balderas Ibarra";
+
+    void message(string name){
+        cout << "My name is " << name <<endl;
+    }
+
+   int main(void){
+       message(myname);
+}
+
